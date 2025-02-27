6d0306e9e396d7e001ef4a047fa9d7110bb81a0c
MatejKrivec <matej.krivec@gmail.com>
2025-02-27 20:07:11 +0100

    Test App runner protected endpoint

--- screens/MyProfile.tsx
+++ screens/MyProfile.tsx
@@ -1,25 +1,50 @@
 import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, TextInput } from 'react-native';
 import axios from 'axios';
 
 const MyProfile = () => {
   const [responseText, setResponseText] = useState('');
+  const [jwtToken, setJwtToken] = useState('');
+  const [users, setUsers] = useState('');
 
-  const fetchData = async () => {
+  const fetchHello = async () => {
     try {
       const response = await axios.get('https://ne3c8kkmwr.eu-central-1.awsapprunner.com/test/hello');
-      setResponseText(response.data); // Update the state with the response data
+      setResponseText(response.data);
     } catch (error) {
       console.error(error);
       setResponseText('Error fetching data');
     }
   };
 
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get('https://ne3c8kkmwr.eu-central-1.awsapprunner.com/profiles', {
+        headers: {
+          Authorization: `Bearer ${jwtToken}`,
+        },
+      });
+      setUsers(JSON.stringify(response.data));
+    } catch (error) {
+      console.error(error);
+      setUsers('Error fetching users');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>This is the My Profile screen!</Text>
-      <Button title="Call Endpoint" onPress={fetchData} />
+      <Button title="Call Hello Endpoint" onPress={fetchHello} />
       <Text style={styles.response}>{responseText}</Text>
+      
+      <TextInput
+        style={styles.input}
+        placeholder="Enter JWT Token"
+        value={jwtToken}
+        onChangeText={setJwtToken}
+      />
+      <Button title="Get Users" onPress={fetchUsers} />
+      <Text style={styles.response}>{users}</Text>
     </View>
   );
 };
@@ -39,6 +64,14 @@ const styles = StyleSheet.create({
     marginTop: 20,
     fontSize: 16,
     color: 'black',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 20,
+    paddingHorizontal: 10,
+    width: '80%',
   },
 });
 
