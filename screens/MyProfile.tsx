fb9abd7e5a938168ed7102810af23602d88c6f76
MatejKrivec <matej.krivec@gmail.com>
2025-02-27 19:53:34 +0100

    Added axios call to test new appRunner endpoint

--- screens/MyProfile.tsx
+++ screens/MyProfile.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import axios from 'axios';
 
 const MyProfile = () => {
+  const [responseText, setResponseText] = useState('');
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://ne3c8kkmwr.eu-central-1.awsapprunner.com/test/hello');
+      setResponseText(response.data); // Update the state with the response data
+    } catch (error) {
+      console.error(error);
+      setResponseText('Error fetching data');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>This is the My Profile screen!</Text>
+      <Button title="Call Endpoint" onPress={fetchData} />
+      <Text style={styles.response}>{responseText}</Text>
     </View>
   );
 };
@@ -19,6 +34,11 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  response: {
+    marginTop: 20,
+    fontSize: 16,
+    color: 'black',
   },
 });
 
